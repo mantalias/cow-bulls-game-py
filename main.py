d9e3c52f34a1b515f6ebb292024737d552a6355b
Alexandros Mantalias <145199456+mantalias@users.noreply.github.com>
2025-06-20 00:20:35 +0300

    Update main.py
    1. Optimized if-else statements that check for cow, bulls score.
    2. Better naming.
    3. Improved the process of checking for invalid input to make it more efficient and cleaner.
    4. Optimized the generation of the number that the user has to guess in each round.

--- main.py
+++ main.py
@@ -1,85 +1,70 @@
-from random import randint
+import random
 
-def generate_random_number():
+def generate_secret_number():
     while True:
-        s = ""
-        for i in range(1,5):            #generates the number
-            s = s + str(randint(1,9))
+        digits = [str(random.randint(0, 9)) for i in range(0, 4)]
+        
+        if len(set(digits)) == len(digits): # Check for duplicates
+            return "".join(digits)
+        
             
-        if duplicate_checker(s) == True:
-            continue
-                                        #checks if the "number" has duplicate digits
-        else:
-            break
-    return s
-               
-            
-def input_user_number(text):
+def get_user_guess(prompt):
     while True:
-        try:
-            x = input(text)                                        #we TRY TO make a variable that points to the integer part of x in order to cover the 3rd case of invalid input
-            y = int(x)
-            if duplicate_checker(x) == True or len(list(x)) != 4 :  #the input from the user cant be with 1.Duplicate Digits or 2.with over four digits or 3. a combination of integers and characters(that is covered above)
-                print("Input a number with 4 different digits\n")
-                continue
-                                            #checks if the "number" has duplicate digits
-            else:
-                break
-        except:
-            print("Invalid Input\n")
+        guess = input(prompt)
+        
+        if not guess.isdigit():
+            print("Invalid input: Please enter only digits.\n")
             continue
-    return x
             
-def duplicate_checker(a_list):
-    
-    a_set = set(a_list)
-    
-    if len(a_set) == len(a_list):
-        return False
-    else:
-        return True
-    
-def cow_bulls(human_guess, number):
-    human_guess = list(human_guess)
-    number = list(number)
-    
+        if len(guess) != 4:
+            print("Invalid input: The number must be 4 digits long.\n")
+            continue
+
+        if has_duplicate_digits(guess):
+            print("Invalid input: The number must have 4 different digits.\n")
+            continue
+            
+        return guess
+            
+def has_duplicate_digits(number_str):
+    return len(set(number_str)) != len(number_str)
+
+def calculate_cows_bulls(secret_number, guess):
     cows = 0
     bulls = 0
     
-    
-    for i in range(0, len(human_guess)):
-        
-        if human_guess[i] in number:                #checks the case of both cow and bulls (that a digit in number justs exists, in whatever index, in the human_guess)
-            if human_guess[i] == number[i]:
-                cows += 1
-                
-            else:
-                bulls += 1
-        else:
-            continue
-        
-    score = [cows, bulls]                                           #the cow_bulls function returns a list with 2 elements consisting of the cows and bulls
-    return score
-        
-log = 0
+    for i in range(0, 4):
+        if guess[i] == secret_number[i]:
+            cows += 1
+        elif guess[i] in secret_number:
+            bulls += 1
+            
+    return cows, bulls 
 
-while True:
-    if log == 0:
-        print("Welcome to Cow and Bulls game!\n")
-              
-    b = generate_random_number()
+def play_game():
+    print("Welcome to Cows and Bulls!\n")
 
     while True:
+        secret_number = generate_secret_number()
+        num_guesses = 0
+
+
+        while True:
+            guess = get_user_guess("Guess a 4-digit number: ")
+            num_guesses += 1
+            
+            cows, bulls = calculate_cows_bulls(secret_number, guess)
+            
+            if cows == 4:
+                print(f"\nCongratulations! You guessed the number {secret_number} in {num_guesses} guesses!\n")
+                break
+            else:
+                print(f"Wrong! Cows: {cows}, Bulls: {bulls}\n")
         
-        a = input_user_number("Guess a number: ")
-        log +=1                                     
-        
-        score = cow_bulls(b, a)
-        
-        
-        if score[0] == 4:
-            print(f"You won!!!(by guessing {log} times)\n")
+        play_again = input("Do you want to play again? (yes/no): ").lower()
+        if play_again != 'yes':
+            print("Thanks for playing! Goodbye.")
             break
-        else:
-            print(f"Wrong! Cows:{score[0]}, Bulls:{score[1]}\n")
-            continue
+
+if __name__ == "__main__":
+    play_game()
